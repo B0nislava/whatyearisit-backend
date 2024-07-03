92aac9c075f94f1e304b381afdd2eca200b2044a
Pénélope <lu.obrad@gmail.com>
2024-07-03 10:11:51 +0200

    deploy me to vercel backend v finale

--- routes/year.js
+++ routes/year.js
@@ -4,9 +4,9 @@ var router = express.Router();
 
 router.get('/', (req, res)=> {
     const date = new Date();
-    res.json({year: '{date.getUTCFullYear()}'});
+    res.json({year: date.getUTCFullYear().toString()});
+    console.log(date)
 });
-
 
 
 module.exports = router;
